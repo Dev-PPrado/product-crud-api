a430b63df265460bcaef5942f7a7f8fe22d077ef
Pedro Prado <pedro.h.s.p@hotmail.com>
2026-09-16 20:21:28 -0300

    feat: Finalizando desenvolvimento do projeto

--- backend/crud.py
+++ backend/crud.py
@@ -1,50 +1,76 @@
 from sqlalchemy.orm import Session
+
 from schemas import ProductUpdate, ProductCreate
 from models import ProductModel
+
 
 def get_products(db: Session):
     """
     Retorna todos os produtos do banco de dados.
     """
     return db.query(ProductModel).all()
-    
+
 
 def get_product(db: Session, product_id: int):
     """
     Retorna um produto específico pelo ID.
     """
-    return db.query(ProductModel).filter(ProductModel.id == product_id).first()
+    return (
+        db.query(ProductModel)
+        .filter(ProductModel.id == product_id)
+        .first()
+    )
+
 
 def create_product(db: Session, product: ProductCreate):
     """
     Cria um novo produto no banco de dados.
     """
-    db_product = ProductModel(**product.model_dump()) # Cria uma instância do modelo com os dados do produto
-    db.add(db_product) # Adiciona o produto ao banco de dados
-    db.commit() # Confirma a transação no banco de dados
-    db.refresh(db_product) # Atualiza o objeto do produto com os dados do banco de dados
-    return db_product # Retorna o objeto do produto criado
+    db_product = ProductModel(**product.model_dump())
+
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+
+    return db_product
+
 
 def delete_product(db: Session, product_id: int):
     """
     Deleta um produto específico pelo ID.
     """
-    db_product = db.query(ProductModel).filter(ProductModel.id == product_id).first()
-    if db_product:
-        db.delete(db_product) # Deleta o produto do banco de dados
-        db.commit() # Confirma a transação no banco de dados
-        return True # Retorna True se o produto foi deletado com sucesso
-    return False # Retorna False se o produto não foi encontrado
+    db_product = (
+        db.query(ProductModel)
+        .filter(ProductModel.id == product_id)
+        .first()
+    )
 
-def update_product(db: Session, product_id: int, product: ProductUpdate):
+    if db_product:
+        db.delete(db_product)
+        db.commit()
+
+        return True
+
+    return False
+
+
+def update_product(
+    db: Session,
+    product_id: int,
+    product: ProductUpdate
+):
     """
     Atualiza um produto específico pelo ID.
     """
-    db_product = db.query(ProductModel).filter(ProductModel.id == product_id).first()
+    db_product = (
+        db.query(ProductModel)
+        .filter(ProductModel.id == product_id)
+        .first()
+    )
 
     if db_product is None:
         return None
-    
+
     if product.name is not None:
         db_product.name = product.name
 
@@ -58,8 +84,9 @@ def update_product(db: Session, product_id: int, product: ProductUpdate):
         db_product.category = product.category
 
     if product.email_fornecedor is not None:
-        db_product.email_fornecedor = product.email_fornecedor  
+        db_product.email_fornecedor = product.email_fornecedor
 
-    db.commit() # Confirma a transação no banco de dados
-    db.refresh(db_product) # Atualiza o objeto do produto com os dados do banco de dados
-    return db_product # Retorna o objeto do produto atualizado
+    db.commit()
+    db.refresh(db_product)
+
+    return db_product
--- backend/router.py
+++ backend/router.py
@@ -1,50 +1,100 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from database import SessionLocal, get_db
-from schemas import ProductResponse, ProductUpdate, ProductCreate
 from typing import List
-from crud import (
-    create_product,
-    get_products,
-    get_product,
-    delete_product,
-    update_product
+from database import get_db
+from schemas import (
+    ProductResponse,
+    ProductUpdate,
+    ProductCreate
 )
+
+from crud import (
+    create_product as crud_create_product,
+    get_products as crud_get_products,
+    get_product as crud_get_product,
+    delete_product as crud_delete_product,
+    update_product as crud_update_product
+)
+
 
 router = APIRouter()
 
-#Rota para buscar todos os itens:
+
+# Rota para buscar todos os produtos
 @router.get("/products/", response_model=List[ProductResponse])
 def read_all_products(db: Session = Depends(get_db)):
-    products = get_products(db)
+    products = crud_get_products(db)
     return products
 
-#Rota para buscar um item:
+
+# Rota para buscar um produto
 @router.get("/products/{product_id}", response_model=ProductResponse)
-def read_one_product(product_id: int, db: Session = Depends(get_db)):
-    db_product = get_products(db=db, product_id=product_id)
+def read_one_product(
+    product_id: int,
+    db: Session = Depends(get_db)
+):
+    db_product = crud_get_product(db, product_id)
 
     if db_product is None:
-        raise HTTPException(status_code=404, detail = "O produto buscado não existe" )
+        raise HTTPException(
+            status_code=404,
+            detail="O produto buscado não existe"
+        )
+
     return db_product
 
-#Rota para adicionar um item:
+
+# Rota para adicionar um produto
 @router.post("/products/", response_model=ProductResponse)
-def create_product(product: ProductCreate, db: Session = Depends(get_db)):
-    return create_product(product= product, db=db)
+def create_product(
+    product: ProductCreate,
+    db: Session = Depends(get_db)
+):
+    return crud_create_product(
+        db=db,
+        product=product
+    )
 
-#Rota para deletar um item:
-@router.delete("/products/{product_id}", response_model=ProductResponse)
-def delete_product(product_id: int, db: Session = Depends(get_db)):
-    product_db =  delete_product(product_id=product_id, db=db)
-    if product_db is None:
-        raise HTTPException(status_code=404, detail = "O produto buscado não existe" )
-    return product_db
 
-#Rota para fazer update:
+# Rota para deletar um produto
+@router.delete("/products/{product_id}")
+def delete_product(
+    product_id: int,
+    db: Session = Depends(get_db)
+):
+    product_deleted = crud_delete_product(
+        db=db,
+        product_id=product_id
+    )
+
+    if not product_deleted:
+        raise HTTPException(
+            status_code=404,
+            detail="O produto buscado não existe"
+        )
+
+    return {
+        "message": "Produto deletado com sucesso"
+    }
+
+
+# Rota para atualizar um produto
 @router.put("/products/{product_id}", response_model=ProductResponse)
-def update_product(product_id: int, product: ProductUpdate, db: Session = Depends(get_db)):
-    product_db = update_product(db=db, product_id=product_id, product=product)
+def update_product(
+    product_id: int,
+    product: ProductUpdate,
+    db: Session = Depends(get_db)
+):
+    product_db = crud_update_product(
+        db=db,
+        product_id=product_id,
+        product=product
+    )
+
     if product_db is None:
-        raise HTTPException(status_code=404, detail = "O produto buscado não existe" )
+        raise HTTPException(
+            status_code=404,
+            detail="O produto buscado não existe"
+        )
+
     return product_db
