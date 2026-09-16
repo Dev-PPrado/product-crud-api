dcd650e6fd7e20b8fab8713bcdcd1c672613c0e7
Pedro Prado <pedro.h.s.p@hotmail.com>
2026-09-16 18:44:10 -0300

    feat: criando programa schema

--- backend/schema.py
+++ backend/schema.py
@@ -13,3 +13,18 @@ class ProductBase(BaseModel):
 class ProductCreate(ProductBase):
     pass
 
+class ProductResponse(ProductBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+class ProductUpdate(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[PositiveFloat] = None
+    category: Optional[str] = None
+    email_fornecedor: Optional[EmailStr] = None
+
+   
